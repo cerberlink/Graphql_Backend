c6131a0320722992f96691c657e8f118c0036185
Carlos <guerreroc47@gmail.com>
2021-03-25 13:21:08 -0700

    first commit for graphql as first time

---
+++ graphql/resolvers/author/index.js
@@ -0,0 +1,7 @@
+
+const authorQueries = {
+    authors: async (_, args) => {},
+    author: async (_, args) => {},
+};
+
+export default authorQueries;
---
+++ graphql/resolvers/author/mutations.js
@@ -0,0 +1,7 @@
+
+const authorMutations = {
+    createAuthor: async (_, args) => { },
+    updateAuthor: async (_, args) => {},
+};
+
+export default authorMutations;
